e308f58beb6fcd1d93dd2c1289cfef23bfe4d040
Nikmet <metlov.n@mail.ru>
2023-02-08 21:07:21 +0300

    Adds comments in person class

--- src/Person.java
+++ src/Person.java
@@ -1,18 +1,24 @@
 public class Person {
+
+    //Поля класса
     String fullName;
     int age;
 
+    //Пустой конструктор
     public Person(){}
 
+    //Конструктор
     public Person(String FIO, int age_person){
         fullName = FIO;
         age = age_person;
     }
 
+    //Метод Move
     public void move(String name){
         System.out.println(name + " идет");
     }
 
+    //Метод Talk
     public void talk(String name){
         System.out.println(name + " говорит");
     }
